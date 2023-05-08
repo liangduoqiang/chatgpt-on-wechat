65f20d6a702c9b91206f35ace86c87a7cb636a15
彭于晏Github分晏 <a296924270@qq.com>
2023-05-08 16:26:45 +0800

    up26

--- plugins/hello/hello.py
+++ plugins/hello/hello.py
@@ -37,12 +37,12 @@ class Hello(Plugin):
             e_context.action = EventAction.CONTINUE  # 事件继续，交付给下个插件或默认逻辑
             return
 
-        if e_context["context"].type == ContextType.PATPAT:
-            e_context["context"].type = ContextType.TEXT
-            msg: ChatMessage = e_context["context"]["msg"]
-            e_context["context"].content = f"请你用中文随机使用一种风格介绍你自己，并告诉用户输入#help可以查看帮助信息。"
-            e_context.action = EventAction.CONTINUE  # 事件继续，交付给下个插件或默认逻辑
-            return
+#         if e_context["context"].type == ContextType.PATPAT:
+#             e_context["context"].type = ContextType.TEXT
+#             msg: ChatMessage = e_context["context"]["msg"]
+#             e_context["context"].content = f"请你用中文随机使用一种风格介绍你自己，并告诉用户输入#help可以查看帮助信息。"
+#             e_context.action = EventAction.CONTINUE  # 事件继续，交付给下个插件或默认逻辑
+#             return
 
         content = e_context["context"].content
         logger.debug("[Hello] on_handle_context. content: %s" % content)
